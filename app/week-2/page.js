80847988a8956f35d1d4bee6f343924a5010cfec
Ray Irving <rayirving.dev@gmail.com>
2026-09-18 23:10:11 -0600

    added week-2 assignment

---
+++ app/week-2/page.js
@@ -0,0 +1,11 @@
+import StudentInfo from "./studentInfo.js";
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <main>
+      <h1>Shopping List</h1>
+      <StudentInfo />
+    </main>
+  );
+}
---
+++ app/week-2/studentInfo.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <h2>Raymond Irving</h2>
+      <Link href="https://github.com/BryanTheGreat01/cprg306-assignments">
+        https://github.com/BryanTheGreat01/cprg306-assignments
+      </Link>
+    </div>
+  );
+}
